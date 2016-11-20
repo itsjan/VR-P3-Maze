45d4a44c1ba71576e12fb1ffbb2d08d0e1a4f5f0
Jan K <jan.kaiponen@gmail.com>
2016-11-20 16:26:11 +0200

    feat: Animate door open

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Door : MonoBehaviour 
 {
+	public bool locked = true;
     // Create a boolean value called "locked" that can be checked in Update() 
 
     void Update() {
         // If the door is unlocked and it is not fully raised
             // Animate the door raising up
+
+		if (!locked) {
+			if (transform.position.y < 19.0f)
+				transform.Translate (0, 2.5f * Time.deltaTime, 0, Space.World);
+		}
     }
 
     public void Unlock()
     {
-        // You'll need to set "locked" to true here
+		locked = false;
     }
 }
