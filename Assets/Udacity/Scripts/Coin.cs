d0ec90768461257803fcd748ea5db08a811b6f7f
Jan K <jan.kaiponen@gmail.com>
2016-11-20 15:05:30 +0200

    feat: Coin script and prefab

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -25,7 +25,6 @@ public class Coin : MonoBehaviour
 	private Color		_color					= Color.white;
 	private float 		_scale					= 1.0f;
 	private float 		_animated_lerp			= 1.0f;
-	private AudioSource _audio_source			= null;
 	private Material	_material				= null;
 
 	[Header("Material")]
@@ -46,20 +45,19 @@ public class Coin : MonoBehaviour
 	public float scale_focus_min				= 0.0f;
 	public float scale_focus_max				= 0.0f;
 
-	[Header("Sounds")]
-	public AudioClip clip_click					= null;	
-
 	[Header("Hide Distance")]
 	public float threshold						= 0.125f;
+
+	[Header("When Clicked")]
+	public GameObject coinPoofPrefab;
+
 
 	void Awake()
 	{		
 		_material					= Instantiate(material);
 		_color_origional			= _material.color;
 		_color						= _color_origional;
-		_audio_source				= gameObject.GetComponent<AudioSource>();	
-		_audio_source.clip		 	= clip_click;
-		_audio_source.playOnAwake 	= false;
+
 	}
 
 	void Update()
@@ -110,13 +108,6 @@ public class Coin : MonoBehaviour
 		_animated_lerp														= Mathf.Abs(Mathf.Cos(Time.time * scale_animation));
 	}
 
-
-    public void OnCoinClicked() {
-        // Instantiate the CoinPoof Prefab where this coin is located
-        // Make sure the poof animates vertically
-        // Destroy this coin. Check the Unity documentation on how to use Destroy
-    }
-
 	public void Enter()
 	{
 		_state = _state == State.Idle ? State.Focused : _state;
@@ -133,11 +124,9 @@ public class Coin : MonoBehaviour
 	{
 		_state = _state == State.Focused ? State.Clicked : _state;
 
+		Object.Instantiate (coinPoofPrefab, gameObject.transform.position, coinPoofPrefab.transform.rotation);
 
-		_audio_source.Play ();
-
-
-		Camera.main.transform.position 	= gameObject.transform.position;
+		Destroy(gameObject);
 	}
 
 
@@ -173,4 +162,5 @@ public class Coin : MonoBehaviour
 		_scale					= Mathf.Lerp(_scale, 		0.0f, lerp_hide);
 		_color					= Color.Lerp(_color, Color.clear, lerp_hide);
 	}
+
 }
