8fafdb786be593fc2dc5833840c8f18f9a731d0c
Jan K <jan.kaiponen@gmail.com>
2016-11-20 19:07:29 +0200

    feat: Add door sounds

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -7,6 +7,8 @@ public class Door : MonoBehaviour
 	public bool locked = true;
 	public bool opened = false;
 	public bool doorIsEnabled = false;
+	public AudioClip[] openCloseSoundFiles;
+	public AudioSource audioSource;
 
     // Create a boolean value called "locked" that can be checked in Update() 
 
@@ -16,19 +18,22 @@ public class Door : MonoBehaviour
 
 		if (!locked && opened) {
 			if (transform.position.y < 20.0f) {
-				transform.Translate (0, 2.5f * Time.deltaTime, 0, Space.World);
+				transform.Translate (0, 10f * Time.deltaTime, 0, Space.World);
 			}
 		} else if (locked || !opened ){
 			if (transform.position.y > 11.5f) {
-				transform.Translate (0, -2.5f * Time.deltaTime, 0, Space.World);
+				transform.Translate (0, -10f * Time.deltaTime, 0, Space.World);
 			}
 		}
 
     }
 
 	public void ToggleDoorOnClick(){
-		if (!locked && doorIsEnabled)
+		if (!locked && doorIsEnabled) {
 			opened = !opened;
+			audioSource.clip = openCloseSoundFiles [opened ? 0 : 1];
+			audioSource.Play ();
+		}
 	}
 
     public void Unlock()
